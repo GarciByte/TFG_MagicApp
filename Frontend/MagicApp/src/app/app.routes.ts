06ede7959529a1bb6592368efdf1ea5a8d5de65e
Noe Gonzalez <noegrelaud@gmail.com>
2025-04-29 20:59:54 +0200

    static chabges on menu and signup + creation of 2 pages, mazos and crear-mazos

--- Frontend/MagicApp/src/app/app.routes.ts
+++ Frontend/MagicApp/src/app/app.routes.ts
@@ -1,34 +1,44 @@
-import { Routes } from '@angular/router';
+import type { Routes } from "@angular/router"
 
 export const routes: Routes = [
   {
-    path: '', // Ruta inicial
-    loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent),
-    pathMatch: 'full'
+    path: "", // Ruta inicial
+    loadComponent: () => import("./pages/home/home.component").then((m) => m.HomeComponent),
+    pathMatch: "full",
   },
   {
-    path: 'login', // Ruta login
-    loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent),
-    pathMatch: 'full'
+    path: "login", // Ruta login
+    loadComponent: () => import("./pages/login/login.component").then((m) => m.LoginComponent),
+    pathMatch: "full",
   },
   {
-    path: 'signup', // Ruta registro
-    loadComponent: () => import('./pages/signup/signup.component').then(m => m.SignupComponent),
-    pathMatch: 'full'
+    path: "signup", // Ruta registro
+    loadComponent: () => import("./pages/signup/signup.component").then((m) => m.SignupComponent),
+    pathMatch: "full",
   },
   {
-    path: 'menu', // Ruta menú principal
-    loadComponent: () => import('./pages/menu/menu.component').then(m => m.MenuComponent),
-    pathMatch: 'full'
+    path: "menu", // Ruta menú principal
+    loadComponent: () => import("./pages/menu/menu.component").then((m) => m.MenuComponent),
+    pathMatch: "full",
   },
   {
-    path: 'card-search', // Ruta del Buscador de Cartas
-    loadComponent: () => import('./pages/card-search/card-search.component').then(m => m.CardSearchComponent),
-    pathMatch: 'full'
+    path: "card-search", // Ruta del Buscador de Cartas
+    loadComponent: () => import("./pages/card-search/card-search.component").then((m) => m.CardSearchComponent),
+    pathMatch: "full",
   },
   {
-    path: '**',
-    redirectTo: '',
-    pathMatch: 'full'
-  }
-];
+    path: "mazos", // Ruta de Mazos
+    loadComponent: () => import("./pages/mazos/mazos.component").then((m) => m.MazosComponent),
+    pathMatch: "full",
+  },
+  {
+    path: "crear-mazo", // Ruta de Crear Mazo
+    loadComponent: () => import("./pages/crear-mazo/crear-mazo.component").then((m) => m.CrearMazoComponent),
+    pathMatch: "full",
+  },
+  {
+    path: "**",
+    redirectTo: "",
+    pathMatch: "full",
+  },
+]
